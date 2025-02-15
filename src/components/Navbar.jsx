bec10b0af3436bba2d449458f94e707f38145448
Antonio50gr <angaro1926@gmail.com>
2025-02-15 16:22:51 +0100

    menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SiFerrari } from "react-icons/si";
+import { AiOutlineClose } from "react-icons/ai";
+import { RiMenuUnfoldFill } from "react-icons/ri";
+
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  const handleChange = () => {
+    setMenu(!menu); // Alterno el estado del menú
+  };
   return (
     <header className='fixed w-full z-10 bg-red-600 py-5'>
         <nav className='container flex justify-between items-center'>
@@ -13,6 +21,13 @@ const Navbar = () => {
                 <a href="" className='hover:text-white transition duration-200 ease-linear'>About us</a>
                 <a href="" className='hover:text-white transition duration-200 ease-linear'>Coches</a>
                 <a href="" className='hover:text-white transition duration-200 ease-linear'>Servicios</a>
+            </div>
+            <div className='md:hidden flex items-center'>
+              {menu ? (
+                <AiOutlineClose size={25} onClick={handleChange}/>
+               ) : (
+               <RiMenuUnfoldFill size={25} onClick={handleChange}/>
+               )}
             </div>
         </nav>
     </header>
