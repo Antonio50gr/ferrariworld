dc9a95bf8b738e13e98a51496b771c351ce30fce
Antonio50gr <angaro1926@gmail.com>
2025-02-19 16:37:07 +0100

    Subiendo cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Cars from './pages/Cars';
+import Gallery from './pages/Gallery';
 
 const App = () => {
   return (
     <>
-    <Navbar/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<About/>}/>
+      <Route path="/" element={<Cars/>}/>
+      <Route path="/" element={<Gallery/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
 
 export default App
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,10 +18,10 @@ const Navbar = () => {
               <a href="" className='font-bold text-3xl text-primary'>Ferrari</a>
             </div>
             <div className='hidden md:flex items-center gap-8 font-medium text-xl'>
-              <a href="" className='hover:text-white transition duration-200 ease-linear'>Home</a>
-              <a href="" className='hover:text-white transition duration-200 ease-linear'>About us</a>
-              <a href="" className='hover:text-white transition duration-200 ease-linear'>Coches</a>
-              <a href="" className='hover:text-white transition duration-200 ease-linear'>Servicios</a>
+              <a href="/" className='hover:text-white transition duration-200 ease-linear'>Home</a>
+              <a href="/about" className='hover:text-white transition duration-200 ease-linear'>About us</a>
+              <a href="/cars" className='hover:text-white transition duration-200 ease-linear'>Coches</a>
+              <a href="/gallery" className='hover:text-white transition duration-200 ease-linear'>Galeria</a>
             </div>
             <div className='md:hidden flex items-center'>
               {menu ? (
